8406516ecfaca0402b282c6e0ed30b09906320bb
edgar <ealejor@fcpn.edu.bo>
2022-05-26 10:20:17 -0400

    edg

---
+++ defensaPersistencia/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace defensaPersistencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("hello");
+        }
+    }
+}
